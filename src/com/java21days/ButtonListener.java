40a1ce54c03b2d7de254c72dd04b2e98b8857306
Alejandro Salas <asalas.wd@outlook.com>
2022-11-19 19:32:54 -0500

    Add method to update label when clicking on butotn

--- src/com/java21days/ButtonListener.java
+++ src/com/java21days/ButtonListener.java
@@ -6,8 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ButtonListener extends JFrame implements ActionListener {
-    Dimension dim = new Dimension(200, 200);
+    Dimension dim = new Dimension(250, 100);
     int clickCounter = 0;
+    JLabel label = new JLabel("Button clicked " + clickCounter + " times");
 
     ButtonListener(String title, String button){
         super(title);
@@ -17,19 +18,19 @@ public class ButtonListener extends JFrame implements ActionListener {
         JPanel panel = new JPanel();
         JButton btn = new JButton(button);
         btn.addActionListener(this);
+        panel.add(label);
         panel.add(btn);
         add(panel);
         setVisible(true);
-        pack();
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         clickCounter++;
         if(clickCounter==1){
-            System.out.println("This has been clicked 1 time");
+            label.setText("This has been clicked 1 time");
         } else {
-            System.out.println("This has been clicked " + clickCounter + " times");
+            label.setText("This has been clicked " + clickCounter + " times");
         }
     }
 }
